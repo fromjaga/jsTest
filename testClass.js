9d28f8ed59dcc5cae2ef3a1e670d9c25fbac7ef2
King <fromjaga@gmail.com>
2014-05-28 18:05:02 +0800

    testClass worked

---
+++ testClass.js
@@ -0,0 +1,12 @@
+(function(window){
+  function testClass(){
+    console.log("js is loaded");
+  }
+
+  testClass.prototype.myMethod=function(){
+    console.log("method tested");
+  <div style="width:120px; height:20px; background:#ffcc33" id="btn"></div>
+    console.log("kdfj");
+  }
+  window.testClass=testClass;
+}(window));
